ea2f88c1bb9c8aabfdd2247bbf59ff7d32254358
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2017-01-16 17:22:41 +0100

    fixed bad merge

--- BookieApp/src/main/java/org/bookie/conf/SpringSecurityConfiguration.java
+++ BookieApp/src/main/java/org/bookie/conf/SpringSecurityConfiguration.java
@@ -1,9 +1,15 @@
 package org.bookie.conf;
 
 import org.apache.commons.lang3.ArrayUtils;
+import com.allanditzel.springframework.security.web.csrf.CsrfTokenResponseHeaderBindingFilter;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.bookie.auth.DatabaseAuthenticationProvider;
 import org.bookie.auth.NoAuthProvider;
 import org.bookie.auth.OrganizationWebAuthenticationDetailsSource;
+import org.bookie.auth.jwt.JwtAuthenticationFilter;
+import org.bookie.auth.jwt.JwtAuthenticationProvider;
+import org.bookie.auth.jwt.NoopAuthenticationEventPublisher;
+import org.bookie.service.UserService;
 import org.bookie.web.rest.RESTAuthenticationEntryPoint;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -12,6 +18,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 import org.springframework.core.env.Environment;
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -19,6 +26,8 @@ import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.security.web.authentication.preauth.AbstractPreAuthenticatedProcessingFilter;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 import org.springframework.security.web.csrf.CsrfFilter;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
@@ -36,6 +45,9 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	@Autowired
 	private RESTAuthenticationEntryPoint authenticationEntryPoint;
 
+	@Autowired
+	private UserService userService;
+
 	@Override
 	protected void configure(final HttpSecurity http) throws Exception {
 		final String[] activeProfiles = this.env.getActiveProfiles();
@@ -45,8 +57,11 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 			final CsrfTokenResponseHeaderBindingFilter csrfTokenFilter = new CsrfTokenResponseHeaderBindingFilter();
 			http.addFilterAfter(csrfTokenFilter, CsrfFilter.class);
 
+			http.csrf().ignoringAntMatchers("/api/auth/token"); //TODO ================ remove me when login is done
+			//TODO curl -X POST -H "Content-Type: application/json" -H "organizationCode: proset" --user "John Doe:secret" http://localhost:8080/api/auth/token
+
 			http.logout()
-					.logoutRequestMatcher(new AntPathRequestMatcher("/api/logout", "GET"))
+					.logoutRequestMatcher(new AntPathRequestMatcher("/api/auth/logout", "GET"))
 					.logoutSuccessUrl("/logged-out.html");
 
 			//		http.authorizeRequests()
@@ -57,17 +72,15 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 
 			http.exceptionHandling().authenticationEntryPoint(this.authenticationEntryPoint);
 
-			//TODO JWT
-			//        http.formLogin()
-			//                .loginProcessingUrl("/api/login/")
-			//                .successHandler(authenticationSuccessHandler)
-			//                .failureHandler(authenticationFailureHandler);
+			http.addFilterBefore(new JwtAuthenticationFilter(authenticationManager()), AbstractPreAuthenticatedProcessingFilter.class)
+					.addFilterBefore(new BasicAuthenticationFilter(authenticationManager()), BasicAuthenticationFilter.class);
 
 			// IMPORTANT
 			http.httpBasic()
 					.authenticationDetailsSource(this.authenticationDetailsSource);
 		}
 	}
+
 
 	//	@Override
 	//	protected void configure(final AuthenticationManagerBuilder auth) throws Exception {
@@ -82,6 +95,23 @@ public class SpringSecurityConfiguration extends WebSecurityConfigurerAdapter {
 	//
 	//
 	//	}
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth
+                .authenticationEventPublisher(new NoopAuthenticationEventPublisher())
+                .authenticationProvider(jwtAuthenticationProvider());
+    }
+
+    @Bean
+    public ObjectMapper objectMapper(){
+	    return new ObjectMapper();
+    }
+
+    @Bean
+    public JwtAuthenticationProvider jwtAuthenticationProvider(){
+        return new JwtAuthenticationProvider();
+    }
 
 	@Bean
 	public WebAuthenticationDetailsSource authenticationDetailsSource() {
