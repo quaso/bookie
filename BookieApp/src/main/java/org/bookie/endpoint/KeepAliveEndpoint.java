24ba1f2809df1dc9116baad60ec422db343c8cb5
Igor Kvasnicka <igor.kvasnicka@annotation.sk>
2017-01-16 17:29:48 +0100

    keep-alive endpoint handles unauthorized requests

--- BookieApp/src/main/java/org/bookie/endpoint/KeepAliveEndpoint.java
+++ BookieApp/src/main/java/org/bookie/endpoint/KeepAliveEndpoint.java
@@ -1,6 +1,9 @@
 package org.bookie.endpoint;
 
+import org.bookie.auth.LoggedUser;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,11 +16,12 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/keep-alive")
 public class KeepAliveEndpoint {
 
-	@RequestMapping(method = RequestMethod.GET, value = "/")
-	@ResponseStatus(HttpStatus.OK)
-	@ResponseBody
-	public Object createOrganization() {
+	@RequestMapping(method = RequestMethod.GET, value = "/", produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity<LoggedUser> createOrganization() {
 		final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-		return authentication != null ? authentication.getPrincipal() : null;
+		if (authentication == null || authentication.getDetails() == null){
+			return new ResponseEntity<>(HttpStatus.UNAUTHORIZED);
+		}
+		return new ResponseEntity<>((LoggedUser) authentication.getDetails(), HttpStatus.OK);
 	}
 }
