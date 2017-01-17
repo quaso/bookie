471f3ff02a765be7e018bb657ab145fd4601bea4
Martin Kvasnicka <Martin.Kvasnicka@firstdata.sk>
2017-01-17 10:07:48 +0100

    application artifact target converted to WAR

--- BookieApp/src/main/java/org/bookie/app/BookieApplication.java
+++ BookieApp/src/main/java/org/bookie/app/BookieApplication.java
@@ -3,13 +3,20 @@ package org.bookie.app;
 import org.bookie.conf.AppConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
 @Import(AppConfiguration.class)
-public class BookieApplication {
+public class BookieApplication extends SpringBootServletInitializer {
 
 	public static void main(final String[] args) {
 		SpringApplication.run(BookieApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
+		return application.sources(BookieApplication.class);
 	}
 }
